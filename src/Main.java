e0436c535a127a05d68de0756b7efd71fd6f9226
noesmc <nikita_sinykovich@mail.ru>
2023-10-18 19:32:55 +0300

    JA_2_1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String str;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Please enter something: ");
+            str = scanner.nextLine();
+            if (str.equalsIgnoreCase("Exit")) {
+                System.out.print("You finally wrote \"Exit\"");
+                break;
+            }
+        }
+    }
+}
